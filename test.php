3901d0e4c940e27a6e261370b516869ce1987ae1
YACINEDAHMOUN <yacine.dahmoun15@gmail.com>
2025-01-04 23:20:56 +0100

    add auth, add login register form contact

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;dbname=ecommerce', 'root', 'root');
-    echo 'Connexion réussie !';
-} catch (PDOException $e) {
-    echo 'Erreur : ' . $e->getMessage();
-}
